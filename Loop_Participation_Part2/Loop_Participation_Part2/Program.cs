216dc35d35dd62704e74f510bf35304f60cadc55
ghern <ghern@DESKTOP-OCUVVH0>
2021-09-23 11:22:54 -0500

    Loop Participation done

---
+++ Loop_Participation_Part2/Loop_Participation_Part2/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Loop_Participation_Part2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double bankaccounts;
+            string answer;
+
+            Console.WriteLine("Hello User. I have a financial question.");
+            do
+            {
+                Console.WriteLine("How many bank accounts do you have? >>");
+                answer = Console.ReadLine();
+
+                if (double.TryParse(answer, out bankaccounts) == false)
+                {
+                    Console.WriteLine($"{answer} is an invalid number. Please try again.");
+                }
+            } while (double.TryParse(answer, out bankaccounts)==false);
+
+            Console.WriteLine("Please enter the amount of each bankaccount");
+            double amount = 0;
+            double average = 0;
+            for (int i = 1; i <= bankaccounts; i++)
+            {
+                Console.WriteLine($"Enter the amount for the {i} account. >>");
+                string amountAsString = Console.ReadLine();
+                amount = amount+ Convert.ToDouble(amountAsString);
+                average = amount / i;
+            }
+
+
+            Console.WriteLine($"Total of all accounts");
+            Console.WriteLine(amount);
+            
+            Console.WriteLine("Average of all accounts");
+            if (average >= 5000)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine(average);
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(average);
+            }
+
+
+        }
+    }
+}
